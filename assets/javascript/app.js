977f600a0c0efc7b7dcb1caa65650feeccd03021
thamma513 <thamma513@gmail.com>
2018-05-05 17:27:16 -0600

    Let's Go

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 var audioElement = document.createElement("audio");
-audioElement.setAttribute("src", "assets/audio/KickstartMyHeart.mp3");
+audioElement.setAttribute("src", "");
 
 $(document).ready(function () {
 
@@ -8,26 +8,117 @@ $(document).ready(function () {
 });
 
 
-function playMusic(){
+function playMusic() {
     audioElement.play();
 }
-
 $(".start-btn").on("click", function(){
     audioElement.pause();
     audioElement.setAttribute("src", "");
-
-    //set new song clip
-    audioElement.setAttribute("src","assets/audio/iWannaRock.mp3");
+    audioElement.setAttribute("src","");
     playMusic();
 });
 
 var intervalID;
 var audio = document.createElement("audio");
+var trivia = {
+    rounds: [{
+            questions: ["Which of these skateboarders was the first person to ollie the El Toro High School stairs?",
+                "What member of the skateboarding company Shortys was also featured in Tony Hawks Pro Skater?",
+                "Who invented the kickflip?", "Who invented the ollie?", "What is the name of the Flip video release in 2002?",
+            ],
+            //round answer options
+            options: [{
+                    choices: ["Rodney Mullen", "Don Nguyen", "Jamie Thomas", "Chris Cole"]
+                }, //question 1 choices
+                {
+                    choices: ["Rodney Mullen", "Jamie Thomas", "Paul Rodriguez", "Chad Muska"]
+                }, //question 2 choices
+                {
+                    choices: ["Rodney Mullen", "Tony Hawk", "Mike McGill", "Rob Dyrdek"]
+                }, //question 3 choices
+                {
+                    choices: ["Mike McGill", "Tony Alva", "Alan Gelfand", "Kenon Sangalang"]
+                },
+                {
+                    choices: ["Really Sorry", "This is Skateboarding", "In Bloom", "Sorry"]
+                },
+            ],
+            answers: ["Don Nguyen", "Chad Muska", "Rodney Mullen", "Alan Gelfand", "Sorry"],
+            answerImages: ["ollie_elToro.gif", "Chad.jpg", "mullen_kickflip.jpg", "ollijpg", "Flip_sorry.jpg"],
+            answerAudio: [".mp3", ".mp3", ".mp3"]
+
+        },
+        //round 2
+        { //Tricks
+            questions: ["?",
+                "? ",
+                "?"
+            ],
+
+            options: [{
+                    choices: ["", "", "", ""]
+                },
+                {
+                    choices: ["", "", "", ""]
+                },
+                {
+                    choices: ["", "", "", ""]
+                }
+            ],
+            answers: ["", "", ""],
+            answerImages: [".jpg", ".jpg", ".jpg"]
+        },
+
+    ],
+    //set initial values
+    currentRound: 0,
+    roundTopics: ["Skateboarding Pop Trivia", "Tricks"],
+    currentQuestion: 0,
+    correctAnswers: 0,
+    incorrectAnswers: 0,
+    timeRemaining: 30,
+}
+
+function clear() {
+    $(".round").empty();
+    $(".question").empty();
+    $(".time-remaining").empty();
+    $(".answer-response").empty();
+    $(".question-options").empty();
+    $(".response-image").empty();
+}
+//function that links to html
+function beginTrivia() {
+
+    //reset rounds to 0
+    // trivia.currentRound = 2;
+    // trivia.currentQuestion = 2;
+    addCss();
+    showQuestion();
+    $(".jumbotron").fadeOut("slow", function () {});
+    $(".trivia-container").fadeTo(2500, 1);
+
+}
+
+function addCss() {
+    $(".trivia-container").css("opacity", 0);
+    $(".trivia-container").addClass("panel panel-default");
+    $(".section-head").addClass("panel-header");
+    $(".section-body").addClass("panel-body");
+}
+$(".start-btn").on("click", function () {
+    audioElement.pause();
+    audioElement.setAttribute("src", "");
+
+    //set new song clip
+    audioElement.setAttribute("src", "mp3");
+    playMusic();
+});
 
 //function to display question
 function showQuestion() {
-   
-    $(".round").html("Round " + (trivia.currentRound + 1) + ": " + trivia.roundTopics[trivia.currentRound]); 
+
+    $(".round").html("Round " + (trivia.currentRound + 1) + ": " + trivia.roundTopics[trivia.currentRound]);
     $(".question").html(trivia.rounds[trivia.currentRound].questions[trivia.currentQuestion]);
     $(".time-remaining").html("Time Remaining: ");
 
@@ -44,7 +135,7 @@ function showQuestion() {
 }
 
 function stopWatch() {
-    
+
     let timeLeft = trivia.timeRemaining--;
 
 
@@ -53,8 +144,8 @@ function stopWatch() {
     } else {
         $(".time-remaining").html("Time is Up!");
         clearInterval(intervalID);
-        trivia.incorrectAnswers++; 
-        let answer = trivia.rounds[trivia.currentRound].answers[trivia.currentQuestion]; 
+        trivia.incorrectAnswers++;
+        let answer = trivia.rounds[trivia.currentRound].answers[trivia.currentQuestion];
         showResponse("Nothing", answer);
     }
 }
@@ -62,8 +153,8 @@ function stopWatch() {
 //USER GUESSES A QUESTION
 $(".question-options").on("click", ".option-btn", function () {
 
-    let guess = ($(this).html()); 
-    let answer = trivia.rounds[trivia.currentRound].answers[trivia.currentQuestion]; 
+    let guess = ($(this).html());
+    let answer = trivia.rounds[trivia.currentRound].answers[trivia.currentQuestion];
     let result;
 
     if (guess === answer) {
@@ -73,7 +164,7 @@ $(".question-options").on("click", ".option-btn", function () {
         trivia.incorrectAnswers++;
         result = "incorrectly";
     }
-    clearInterval(intervalID); 
+    clearInterval(intervalID);
     showResponse(result, answer);
 });
 
@@ -85,20 +176,20 @@ function showResponse(result, answer) {
         $(".answer-response").html("You answered <b style= color:#B20000>" + result + "</b>! The correct answer was " + answer + ".");
     }
 
- 
+
     $(".question-options").empty();
 
     let answerImage = $("<img>");
     answerImage.addClass("answerImg");
 
     if (trivia.currentRound === 0) {
-        let audioAddress = "assets/audio/" + trivia.rounds[trivia.currentRound].answerAudio[trivia.currentQuestion];
+        let audioAddress = "audio/" + trivia.rounds[trivia.currentRound].answerAudio[trivia.currentQuestion];
         audio.setAttribute("src", audioAddress);
         audio.play();
     }
 
 
-    let imageAddress = "assets/images/" + trivia.rounds[trivia.currentRound].answerImages[trivia.currentQuestion];
+    let imageAddress = "" + trivia.rounds[trivia.currentRound].answerImages[trivia.currentQuestion];
 
     answerImage.attr("src", imageAddress);
     $(".response-image").append(answerImage);
@@ -127,16 +218,16 @@ function timeToNextQuestion() {
 
 function nextQuestion() {
 
-    if (trivia.currentQuestion === 4) { 
-        if (trivia.currentRound === 4) { 
+    if (trivia.currentQuestion === 4) {
+        if (trivia.currentRound === 1) {
             showScores();
             return;
         } else {
-            trivia.currentRound++; 
-            trivia.currentQuestion = 0; 
+            trivia.currentRound++;
+            trivia.currentQuestion = 0;
         }
     } else {
-        trivia.currentQuestion++; 
+        trivia.currentQuestion++;
     }
 
     reset();
@@ -150,7 +241,7 @@ function reset() {
 
 }
 var audioEnd = document.createElement("audio");
-audioEnd.setAttribute("src", "assets/audio/thriller.mp3");
+audioEnd.setAttribute("src", ".mp3");
 
 function showScores() {
 
@@ -170,10 +261,10 @@ function showScores() {
 
     let restartButton = $('<button>', {
         class: 'replayBtn',
-        on   : {
-           click: function() {
-               replayTrivia();
-           }
+        on: {
+            click: function () {
+                replayTrivia();
+            }
         }
     });
 
@@ -191,64 +282,34 @@ function showScores() {
 
 }
 
-function replayTrivia(){
+function replayTrivia() {
     trivia.currentQuestion = 0;
     trivia.currentRound = 0;
     trivia.correctAnswers = 0;
     trivia.incorrectAnswers = 0;
     trivia.timeRemaining = 30;
-    
+
     audioEnd.pause();
     audioEnd.setAttribute("src", "");
 
     //set new song clip
-    audioEnd.setAttribute("src","assets/audio/iWannaRock.mp3");
+    audioEnd.setAttribute("src", ".mp3");
     playMusic();
 
     clear();
     $(".trivia-container").css("opacity", 0);
-    $( ".trivia-container" ).fadeTo( 2000, 1);
+    $(".trivia-container").fadeTo(2000, 1);
     showQuestion();
 }
-
-function clear(){
-    $(".round").empty();
-    $(".question").empty();
-    $(".time-remaining").empty();
-    $(".answer-response").empty();
-    $(".question-options").empty();
-    $(".response-image").empty();
-}
-//function that links to html
-function beginTrivia() {
-
-    //reset rounds to 0
-    // trivia.currentRound = 2;
-    // trivia.currentQuestion = 2;
-    addCss();
-    showQuestion();
-    $( ".jumbotron" ).fadeOut( "slow", function(){});
-    $( ".trivia-container" ).fadeTo( 2500, 1);
-
-}
-
-function addCss(){
-    $(".trivia-container").css("opacity", 0);
-    $(".trivia-container").addClass("panel panel-default");
-    $(".section-head").addClass("panel-header");
-    $(".section-body").addClass("panel-body");
-}
-
 var trivia = {
-    rounds: [
-        { 
+    rounds: [{
             questions: ["Which of these skateboarders was the first person to ollie the El Toro High School stairs?",
-            "What member of the skateboarding company Shortys was also featured in Tony Hawks Pro Skater?",
-            "Who invented the kickflip?","Who invented the ollie?","What is the name of the Flip video release in 2002?",
+                "What member of the skateboarding company Shortys was also featured in Tony Hawks Pro Skater?",
+                "Who invented the kickflip?", "Who invented the ollie?", "What is the name of the Flip video release in 2002?",
             ],
             //round answer options
             options: [{
-                    choices: ["Rodney Mullen","Don Nguyen", "Jamie Thomas", "Chris Cole"]
+                    choices: ["Rodney Mullen", "Don Nguyen", "Jamie Thomas", "Chris Cole"]
                 }, //question 1 choices
                 {
                     choices: ["Rodney Mullen", "Jamie Thomas", "Paul Rodriguez", "Chad Muska"]
@@ -257,15 +318,15 @@ var trivia = {
                     choices: ["Rodney Mullen", "Tony Hawk", "Mike McGill", "Rob Dyrdek"]
                 }, //question 3 choices
                 {
-                    choices: ["Mike McGill","Tony Alva","Alan Gelfand","Kenon Sangalang"]
+                    choices: ["Mike McGill", "Tony Alva", "Alan Gelfand", "Kenon Sangalang"]
                 },
                 {
-                    choices:["Really Sorry","This is Skateboarding","In Bloom","Sorry"]
+                    choices: ["Really Sorry", "This is Skateboarding", "In Bloom", "Sorry"]
                 },
             ],
-            answers: ["Don Nguyen","Chad Muska", "Rodney Mullen","Alan Gelfand","Sorry"],
-            answerImages: ["images/ollie_elToro.gif", "images/Chad.jpg", "images/mullen_kickflip.jpg","images/ollijpg","images/Flip_sorry.jpg"],
-            answerAudio:["justWhatINeeded.mp3","welcomeToTheJungle.mp3","dontStopBelievin.mp3"]
+            answers: ["Don Nguyen", "Chad Muska", "Rodney Mullen", "Alan Gelfand", "Sorry"],
+            answerImages: ["Trivia-Game/images/ollie_elToro.gif", "Trivia-Game/images/Chad.jpg", "Trivia-Game/images/mullen_kickflip.jpg", "Trivia-Game/images/ollijpg", "Trivia-Game/images/Flip_sorry.jpg"],
+            answerAudio: [".mp3", ".mp3", ".mp3"]
 
         },
         //round 2
@@ -274,7 +335,7 @@ var trivia = {
                 "? ",
                 "?"
             ],
-            
+
             options: [{
                     choices: ["", "", "", ""]
                 },
@@ -282,13 +343,13 @@ var trivia = {
                     choices: ["", "", "", ""]
                 },
                 {
-                    choices: ["30", "26", "40", "23"]
+                    choices: ["", "", "", ""]
                 }
             ],
-            answers: ["Detention", "Harrison Ford", "23"],
-            answerImages: ["breakfastClub.jpg", "harrisonFord.jpg", "eddyMurphy.jpg"]
+            answers: ["", "", ""],
+            answerImages: [".jpg", ".jpg", ".jpg"]
         },
-       
+
     ],
     //set initial values
     currentRound: 0,
